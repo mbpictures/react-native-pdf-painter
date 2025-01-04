b891c27557ec3b7c71e309bbf6d816a9275d7cbd
Marius Butz <mbpictures13@gmail.com>
2025-01-04 14:18:05 +0100

    style: set tab width to 4

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,22 +2,22 @@ import { View, StyleSheet } from 'react-native';
 import { PdfAnnotationView } from 'react-native-pdf-annotation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <PdfAnnotationView color="#32a852" style={styles.box} />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <PdfAnnotationView color="#32a852" style={styles.box} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    box: {
+        width: 60,
+        height: 60,
+        marginVertical: 20,
+    },
 });
