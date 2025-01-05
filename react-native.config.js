cf1b1c26eb314e18426c485ba4621d5214d7aaab
Marius Butz <mbpictures13@gmail.com>
2025-01-05 15:42:09 +0100

    chore: fix lint errors

--- react-native.config.js
+++ react-native.config.js
@@ -2,11 +2,11 @@
  * @type {import('@react-native-community/cli-types').UserDependencyConfig}
  */
 module.exports = {
-  dependency: {
-    platforms: {
-      android: {
-        cmakeListsPath: 'generated/jni/CMakeLists.txt',
-      },
+    dependency: {
+        platforms: {
+            android: {
+                cmakeListsPath: 'generated/jni/CMakeLists.txt',
+            },
+        },
     },
-  },
 };
